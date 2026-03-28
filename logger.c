845ffeb4f0ffb6669bf431dc030bd67c66507ec5
Sakshi0572 <sakahishinde10@gmail.com>
2026-03-28 18:06:37 +0530

    Implement FIFO logging in start_logger function
    Refactored logging functionality to read from FIFO and log data continuously.

--- logger.c
+++ logger.c
@@ -3,23 +3,60 @@
    -----------------------------------------
    Logs order processing results into file.
 */
-#include <stdio.h>
-#include "logger.h"
-#include <errno.h>
-void log_order(int product_id, int quantity, int status) {
-	// Open log file in append mode
-	FILE *fp = fopen("orders.log", "a");
-	
-	if(fp == NULL) {
-		printf("File open failed with error # %d\n" , errno);
-		perror("Error : ");
-	}
-	// Write result into file
-	if(status) {
-		fprintf(fp, "Order %d Success Qunatity : %d\n", product_id, quantity);
-	}
-	else{
-		fprintf(fp, "Order %d Failed Qunatity : %d\n", product_id, quantity);
-	}
-	fclose(fp);   //Close file
+
+#include <stdio.h>      
+#include <fcntl.h>      
+#include <unistd.h>     
+#include <string.h>     
+#include <errno.h>      
+#include "logger.h"     
+
+//start logger process
+void start_logger() {
+
+    char buffer[100];   // Buffer to store data read from FIFO
+
+    // Open FIFO (named pipe) 
+    int fifo_fd = open("myfifo", O_RDONLY);
+
+    // Check if FIFO open failed
+    if(fifo_fd == -1){
+        printf("Fifo is failed with error # %d\n", errno);
+        perror("Error : ");
+    }
+
+    // continuously read data
+    while(1){
+
+        // Read data from FIFO into buffer
+        ssize_t readBytes = read(fifo_fd, buffer, sizeof(buffer));
+
+        // Check if read failed
+        if(readBytes == -1){
+            printf("Read failed with error # %d\n", errno);
+            perror("Error: ");
+        }
+
+        // Open log file in append mode ("a" → adds data without deleting old data)
+        FILE *fp = fopen("orders.log", "a");
+
+        // Check if file opening failed
+        if(fp == NULL) {
+            printf("File open failed with error # %d\n" , errno);
+            perror("Error : ");
+        }
+        else {
+            // Write buffer data into file
+            fprintf(fp, "%s", buffer);
+
+            // Close file after writing
+            fclose(fp);
+        }
+
+        // Print logged data on console
+        printf("Logged: %s", buffer);
+    }
+
+    // Close FIFO 
+    close(fifo_fd);
 }
